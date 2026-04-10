3f9ecb2d7477a033cf574df21e3c64a9db383790
Admin <admin@charity.com>
2026-04-10 20:32:30 +0200

    Fix: correct apiKey and database URL

--- js/config/firebase.js
+++ js/config/firebase.js
@@ -3,11 +3,12 @@ import { getDatabase, ref, push, onValue, remove, update } from "https://www.gst
 import { getAuth, signInWithEmailAndPassword, signOut, updatePassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBJXx6S96j90hMYNXerxNx7vXQ8WcuCj3dg",
+    apiKey: "AIzaSyBJx6S96j90hMYNXerxNx7vXQ8WcuCj3dg",
     authDomain: "charitysystem-5c4d9.firebaseapp.com",
+    databaseURL: "https://charitysystem-5c4d9-default-rtdb.firebaseio.com",
     projectId: "charitysystem-5c4d9",
     storageBucket: "charitysystem-5c4d9.firebasestorage.app",
-    messagingSenderId: "4760375130441",
+    messagingSenderId: "476037513041",
     appId: "1:476037513041:web:c74b9cd103fb4ee8911d70",
     measurementId: "G-DLQTK078R9"
 };
